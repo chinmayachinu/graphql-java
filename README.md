2afc9153a634091e908800f091b67f39e17f4802
Chinmaya Kumar Panigrahi <chinmaya.panigrahi@booking.com>
2023-06-19 20:27:37 +0530

    added graphql kick stater and related code

--- README.md
+++ README.md
@@ -9,3 +9,62 @@
  7. PropertyDataFetcher fetches data from Map and Java Beans.
  8. So when the field name matches the Map key or the property name of the source Object, no DataFetcher is needed.
  9. Type resolver is used to resolve the type
+
+
+# My learning
+
+## Basic 
+ 1. A GraphQL service is created by defining types and fields on those types.
+ 2. Then providing functions for each field on each type.
+
+e.g. 
+
+`type Query{
+    me: User
+}`
+
+`type User {
+    id: ID
+    name: String
+}`
+
+Along with functions for each field on each type:
+
+`function Query_me(request) {
+    return request.auth.user
+}`
+
+`function User_name(user) {
+    return user.getName()
+}`
+
+For example, the query:
+
+`{
+    me {
+        name
+    }
+}`
+
+Could produce the following JSON result:
+
+`{
+    "me": {
+        "name": "Luke Skywalker"
+    }
+}`
+
+### The Query and Mutation types
+
+Most types in your schema will just be normal object types, but there are two types that are special within a schema:
+
+`schema {
+    query: Query
+    mutation: Mutation
+}`
+
+Every GraphQL service has a query type and may or may not have a mutation type. These types are the same as a regular object type, but they are special because they define the entry point of every GraphQL query.
+
+### Note : Data fetchers are sometimes called "resolvers" in other graphql implementations.
+
+### Note : A TypeResolver helps graphql-java to decide which type a concrete value belongs to. This is needed for Interface and Union.
--- src/main/java/com/chinmaya/graphqldemo/GraphqldemoApplication.java
+++ src/main/java/com/chinmaya/graphqldemo/GraphqldemoApplication.java
@@ -1,13 +1,117 @@
 package com.chinmaya.graphqldemo;
 
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.kickstart.tools.GraphQLResolver;
+import graphql.kickstart.tools.SchemaParser;
+import graphql.kickstart.tools.SchemaParserBuilder;
+import graphql.schema.GraphQLSchema;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class GraphqldemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraphqldemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqldemoApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(Query query, BookResolver bookResolver) {
+
+        SchemaParserBuilder file = SchemaParser
+                .newParser()
+                .file("graphql/schema.graphqls")
+                .resolvers(query, bookResolver);
+
+        System.out.println("Schema parser builder => " + file);
+        SchemaParser schemaParser = file.build();
+        System.out.println("Schema parser => " + schemaParser);
+
+        GraphQLSchema graphQLSchema = schemaParser.makeExecutableSchema();
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = build.execute("""
+                query {
+                   books {
+                     id
+                     authorId
+                     title
+                     coverImage
+                     pages
+                     releaseDate
+                     isbn
+                   }
+                 }
+                """);
+
+        System.out.println(executionResult.getData().toString());
+
+        return args -> System.out.println("application started");
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    record Book(int id, int authorId, String title, String coverImage, int pages, String releaseDate, String isbn) {
+    }
+
+    @Component
+    static class BookResolver implements GraphQLResolver<Book> /* This class is a resolver for the Book "Data Class" */ {
+
+        public BookResolver() {
+        }
+
+//        public BookResolver(AuthorRepository authorRepository) {
+//            this.authorRepository = authorRepository;
+//        }
+
+//        public Author author(Book book) {
+//            return authorRepository.findById(book.getAuthorId());
+//        }
+    }
+
+    @Component
+    class Query implements GraphQLQueryResolver {
+
+        @Autowired
+        private final RestTemplate restTemplate;
+
+        Query(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
+
+        public List<Book> books() {
+
+            String url = "https://my-json-server.typicode.com/dmitrijt9/book-api-mock/books";
+            List<Book> books = null;
+
+            ResponseEntity<List<Book>> response = restTemplate.exchange(url, HttpMethod.GET, null,
+                    new ParameterizedTypeReference<>() {
+                    });
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                books = response.getBody();
+                for (Book book : books) {
+                    System.out.println(book.toString());
+                }
+            } else {
+                System.out.println("Error: " + response.getStatusCode());
+            }
+            return books;
+        }
+    }
 
 }
